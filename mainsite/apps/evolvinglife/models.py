2033b90ce8bbe2de113578bc99cb80caa64498a2
Jack <songjiapei@gmail.com>
2014-09-20 15:25:18 -0400

    added model tostring methods

--- mainsite/apps/evolvinglife/models.py
+++ mainsite/apps/evolvinglife/models.py
@@ -7,17 +7,29 @@ class User(models.Model):
 	symbol = models.CharField(max_length=1, unique=True)
 	time = models.DateTimeField(auto_now_add=True)
 
+	def __unicode__(self):
+		return self.symbol
+
 class UserPoint(models.Model):
 	x = models.IntegerField()
 	y = models.IntegerField()
 	user = models.ForeignKey(User)
+
+	def __unicode__(self):
+		return self.x + "," + self.y + " : " + self.symbol
 
 class Geography(models.Model):
 	category = models.TextField(unique=True)
 	colour = models.IntegerField(unique=True)
 	symbol = models.CharField(max_length=1, unique=True)
 
+	def __unicode__(self):
+		return self.symbol
+
 class GeographyPoint(models.Model):
 	x = models.IntegerField()
 	y = models.IntegerField()
 	geography = models.ForeignKey(Geography)
+
+	def __unicode__(self):
+		return self.x + "," + self.y + " : " + self.symbol
